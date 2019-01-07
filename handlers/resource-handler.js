cee5263b0d2056bd6b68c90ca0b3bda1dc791caf
cuongdq.payment <cuongdq.payment@gmail.com>
2019-01-07 17:14:35 +0700

    cuongdq push by sh

--- handlers/resource-handler.js
+++ handlers/resource-handler.js
@@ -8,6 +8,7 @@ var exceltojson; //ham phu thuoc vao xlsx/xls
 
 const PDFDocument = require('pdfkit');
 const fs = require('fs');
+const url = require('url');
 
 const vnd = require('../utils/number-2-vnd');
 
@@ -148,10 +149,10 @@ var createExcel = (jsonRows)=>{
 
 }
 
-var createPdfInvoices = (jsonRows)=>{
+var createPdfInvoices = (jsonRows,outputFilename)=>{
     //result la array ket qua can phai in ra hoa don
     var doc = new PDFDocument();
-    var outputFilename = './pdf/hoadon_all.pdf';
+    
     var stream = doc.pipe(fs.createWriteStream(outputFilename));
 
     doc.info['Title'] = 'Hóa đơn thu tiền rác';
@@ -165,7 +166,8 @@ var createPdfInvoices = (jsonRows)=>{
     jsonRows.forEach((el,idx) => {
         let charge = parseInt(el.charge);
         if (idx>0) doc.addPage();
-        doc.text(el.cus_id, 100);
+
+        doc.text(el.cust_id, 100);
         doc.moveDown(0.5)
         doc.text(el.full_name);
         doc.moveDown(0.5)
@@ -181,7 +183,7 @@ var createPdfInvoices = (jsonRows)=>{
 
         doc.moveDown(18);
 
-        doc.text(el.cus_id);
+        doc.text(el.cust_id);
         doc.moveDown(0.5)
         doc.text(el.full_name);
         doc.moveDown(0.5)
@@ -198,15 +200,8 @@ var createPdfInvoices = (jsonRows)=>{
     
     doc.end();
 
-    stream.on('finish', () =>{
-        //console.log(stream);
-        fs.readFile(outputFilename, { flag: 'r' }, (error, bufferPdf)=>{
-            if (err) {
-                throw err;
-            } 
-            return bufferPdf;
-        });
-    });
+    return stream;
+
 }
 
 class ResourceHandler {
@@ -333,139 +328,6 @@ class ResourceHandler {
         }
     }
 
-    getPdfInvoice(req, res, next) {
-        var doc = new PDFDocument();
-        var outputFilename = './pdf/hoadon.pdf';
-        var stream = doc.pipe(fs.createWriteStream(outputFilename));
-
-        doc.info['Title'] = 'Hoa don';
-        doc.info['Author'] = 'Cuong.dq';
-        doc.registerFont('Palatino', 'fonts/PalatinoBold.ttf');
-
-        var lorem = 'Ho va ten'
-
-        doc.fontSize(8);
-
-        doc.text('This text is left aligned. ' + lorem,
-            {
-                width: 410,
-                align: 'left'
-            }
-        )
-
-        doc.moveDown()
-        doc.text('This text is centered. ' + lorem, {
-            width: 410,
-            align: 'center'
-        })
-
-        doc.end();
-
-        stream.on('finish', () =>{
-            //console.log(stream);
-            fs.readFile(outputFilename, { flag: 'r' }, function (error, data) {
-                if (!error) {
-                    res.writeHead(200, { 'Content-Type': 'application/pdf; charset=utf-8' });
-                    res.end(data);
-                } else {
-                    res.writeHead(404, { 'Content-Type': 'text/html; charset=utf-8' });
-                    res.end(JSON.stringify(error));
-                }
-            });
-        });
-    }
-
-
-
-    getPdfInvoicesFromExcel(req, res, next) {
-
-        var inputFilename = './db/ql-hoadon.xlsx';
-        exceltojson = xlsxtojson;
-        try {
-            exceltojson({
-                input: inputFilename,
-                output: null, //since we don't need output.json
-                lowerCaseHeaders: true
-            }, function (err, result) {
-                if (err) {
-                    throw err;
-                }
-                //result la array ket qua can phai in ra hoa don
-                var doc = new PDFDocument();
-                var outputFilename = './pdf/hoadon_all.pdf';
-                var stream = doc.pipe(fs.createWriteStream(outputFilename));
-        
-                doc.info['Title'] = 'Hóa đơn thu tiền rác';
-                doc.info['Author'] = 'Qld Vinh Hưng';
-                
-                //doc.registerFont('Tahoma-utf8', './fonts/tahoma.ttf');
-                //doc.font('Tahoma-utf8');
-
-                doc.registerFont('Time-new-roman-utf8', './fonts/times.ttf');
-                doc.font('Time-new-roman-utf8');
-
-                doc.fontSize(13);
-                
-                //{"status":"","stt":"1","full_name":"Phan Thanh Hiệp","type":"Hộ KH Bình Thường","tax_id":"","charge":"20,000","area":"Thôn Lương Viện","address":"Thôn Lương Viện","phone":"","staff":"Bốn","cus_id":"R000000001","last_name":"Phan Thanh","first_name":"Hiệp","type_id":"1","price_id":"1","area_id":"1","staff_id":"1","start_date":"01/01/2018","end_date":"","change_date":""}
-
-                result.forEach((el,idx) => {
-
-                    let charge = parseInt(el.charge.replace(',',''));
-
-                    if (idx>0) doc.addPage();
-                    doc.text(el.cus_id, 100);
-                    doc.moveDown(0.5)
-                    doc.text(el.full_name);
-                    doc.moveDown(0.5)
-                    doc.text(el.address);
-                    doc.moveDown(0.5)
-                    doc.text(el.tax_id);
-                    doc.moveDown(0.5)
-                    doc.text(charge);
-                    doc.moveDown(0.5)
-                    doc.text(vnd.DocTienBangChu(charge));
-                    doc.moveDown()
-                    doc.text(el.staff);
-
-                    
-                    doc.moveDown(18);
-
-                    doc.text(el.cus_id);
-                    doc.moveDown(0.5)
-                    doc.text(el.full_name);
-                    doc.moveDown(0.5)
-                    doc.text(el.address);
-                    doc.moveDown(0.5)
-                    doc.text(el.tax_id);
-                    doc.moveDown(0.5)
-                    doc.text(charge);
-                    doc.moveDown(0.5)
-                    doc.text(vnd.DocTienBangChu(charge));
-                    doc.moveDown(0.5)
-                    doc.text(el.staff);
-                });
-                
-                doc.end();
-        
-                stream.on('finish', () =>{
-                    //console.log(stream);
-                    fs.readFile(outputFilename, { flag: 'r' }, function (error, data) {
-                        if (!error) {
-                            res.writeHead(200, { 'Content-Type': 'application/pdf; charset=utf-8' });
-                            res.end(data);
-                        } else {
-                            res.writeHead(404, { 'Content-Type': 'text/html; charset=utf-8' });
-                            res.end(JSON.stringify(error));
-                        }
-                    });
-                });
-            });
-        } catch (e) {
-            throw "Corupted excel file";
-        }
-
-    }
-
     /**
      * Doc csdl lay data tao excel va tra file backup
      * 
@@ -568,7 +430,79 @@ class ResourceHandler {
             });
     }
 
+    getPdfInvoice(req, res, next) {
+        //lay duong dan va lay ma khach hang tren duong dan
+        // /db/pdf-invoice/<cust_id>
+        let path = decodeURIComponent(url.parse(req.url, true, false).pathname);
+        let cust_id = path.substring('/pdf-invoice/'.length);
+        // console.log('path: ',path);
+        // console.log('cust_id: ',cust_id);
+        let sql='select \
+                    a.stt\
+                    ,a.full_name\
+                    ,a.tax_id\
+                    ,a.address\
+                    ,a.email\
+                    ,a.phone\
+                    ,a.cust_id\
+                    ,a.last_name \
+                    ,a.first_name \
+                    ,a.type_id\
+                    ,b.description as cust_type\
+                    ,b.value as charge\
+                    ,a.price_id\
+                    ,a.area_id\
+                    ,c.description as area\
+                    ,a.staff_id\
+                    ,d.description as staff\
+                    ,a.start_date\
+                    ,a.end_date\
+                    ,a.change_date\
+                    ,a.status\
+                from customers a\
+                ,(select code, description, value from parameters where type = 5) b\
+                ,(select code, description from parameters where type = 6) c\
+                ,(select code, description from parameters where type = 4) d\
+                where\
+                a.stt=\''+cust_id+'\'\
+                and a.type_id = b.code\
+                and a.area_id = c.code\
+                and a.staff_id = d.code\
+                order by stt\
+                ';
+        //console.log(sql);    
+        db.db.getRsts(sql)
+            .then(results=>{
+
+              let outputFilename = './pdf/hoadon_'+cust_id+'.pdf';
+              let stream = createPdfInvoices(results,outputFilename);
+              
+              stream.on('finish', () =>{
+                    fs.readFile(outputFilename, { flag: 'r' }, (err, bufferPdf)=>{
+                        if (err) {
+                            res.writeHead(404, { 'Content-Type': 'text/html; charset=utf-8' });
+                            res.end(JSON.stringify(err));
+                        }
+                        res.writeHead(200, { 'Content-Type': 'application/pdf; charset=utf-8' });
+                        res.end(bufferPdf);
+                    });
+                });
+            })
+            .catch(err=>{
+                //console.log('err',err);
+                res.writeHead(404, { 'Content-Type': 'text/html; charset=utf-8' });
+                res.end(JSON.stringify(err));
+            });
+    }
+
+    /**
+     * 
+     * @param {*} req 
+     * @param {*} res 
+     * @param {*} next 
+     */
     getPdfInvoices(req, res, next){
+
         db.db.getRsts('select \
                         a.stt\
                         ,a.full_name\
@@ -601,10 +535,20 @@ class ResourceHandler {
                       order by stt\
                       ')
             .then(results=>{
-              let pdfBuffer = createPdfInvoices(results);
-               //show file pdf
-               res.writeHead(200, { 'Content-Type': 'application/pdf; charset=utf-8' });
-               res.end(pdfBuffer);
+
+              let outputFilename = './pdf/hoadon_all.pdf';
+              let stream = createPdfInvoices(results,outputFilename);
+              
+              stream.on('finish', () =>{
+                    fs.readFile(outputFilename, { flag: 'r' }, (err, bufferPdf)=>{
+                        if (err) {
+                            res.writeHead(404, { 'Content-Type': 'text/html; charset=utf-8' });
+                            res.end(JSON.stringify(err));
+                        }
+                        res.writeHead(200, { 'Content-Type': 'application/pdf; charset=utf-8' });
+                        res.end(bufferPdf);
+                    });
+                });
             })
             .catch(err=>{
                 res.writeHead(404, { 'Content-Type': 'text/html; charset=utf-8' });
--- routes/resource-excel.js
+++ routes/resource-excel.js
@@ -5,7 +5,7 @@ let handlers = resourceHandler.ResourceHandler;
 //kiem tra token dung
 router.get('/json-parameters', handlers.getParameters);
 router.get('/json-customers', handlers.getCustomers);
-router.get('/pdf-invoice', handlers.getPdfInvoice);
+router.get('/pdf-invoice/*', handlers.getPdfInvoice);
 router.get('/pdf-invoices', handlers.getPdfInvoices);
 router.get('/file-customers', handlers.getAttachCustomers);
 router.get('/excel-customers', handlers.viewExcelCustomers);
