f0049b641fc3c7f8a7608df7e0ecd97849b1f6a2
cuongdq.payment <cuongdq.payment@gmail.com>
2019-01-13 07:52:22 +0700

    cuongdq push by sh

--- create-sqlite-from-excel-setting.js
+++ create-sqlite-from-excel-setting.js
@@ -5,39 +5,39 @@ const utils = require('./utils/converter-2-vietnamese');
 
 
 var billPrintMatrix = {
-    bill_date: [{col:285,row:65},{col:340,row:65},{col:375,row:65}],
-    invoice_no:{col:480,row:55},
-    full_name:{col:160,row:90},
-    organization_name:{col:120,row:105},
-    tax_no: {col:120,row:120},
-    address:{col:120,row:135},
-    pay_method:{col:145,row:148},
-    account_no:{col:310,row:148},
-    bill_details:[
+    bill_date: [{ col: 285, row: 65 }, { col: 340, row: 65 }, { col: 375, row: 65 }],
+    invoice_no: { col: 480, row: 55 },
+    full_name: { col: 160, row: 90 },
+    organization_name: { col: 120, row: 105 },
+    tax_no: { col: 120, row: 120 },
+    address: { col: 120, row: 135 },
+    pay_method: { col: 145, row: 148 },
+    account_no: { col: 310, row: 148 },
+    bill_details: [
         {
-        unit: {col:280,row:185},
-        product_count: {col:260,row:185, width: 100, align: 'right', color:'red'},
-        price_not_vat: {col:350,row:185, width: 100, align: 'right', color:'red'},
-        total_not_vat: {col:450,row:185, width: 100, align: 'right', color:'red'}
+            unit: { col: 280, row: 185 },
+            product_count: { col: 260, row: 185, width: 100, align: 'right', color: 'red' },
+            price_not_vat: { col: 350, row: 185, width: 100, align: 'right', color: 'red' },
+            total_not_vat: { col: 450, row: 185, width: 100, align: 'right', color: 'red' }
         }
     ],
-    bill_sum:{
-        sum_not_vat:{col:450,row:218, width: 100, align: 'right', color:'red'},
-        sum_vat:{col:450,row:236, width: 100, align: 'right', color:'red'},
-        sum_charge: {col:450,row:253, width: 100, align: 'right', color:'red'}
+    bill_sum: {
+        sum_not_vat: { col: 450, row: 218, width: 100, align: 'right', color: 'red' },
+        sum_vat: { col: 450, row: 236, width: 100, align: 'right', color: 'red' },
+        sum_charge: { col: 450, row: 253, width: 100, align: 'right', color: 'red' }
     },
-    bill_sum_charge_spell: {col:155,row:273, color:'red'},
-    sign_customer:{
-        signature:{col:120,row:330},
-        full_name:{col:110,row:375}
+    bill_sum_charge_spell: { col: 155, row: 273, color: 'red' },
+    sign_customer: {
+        signature: { col: 120, row: 330 },
+        full_name: { col: 110, row: 375 }
     },
-    sign_saler:{
-        signature:{col:290,row:330},
-        full_name:{col:280,row:375}
+    sign_saler: {
+        signature: { col: 290, row: 330 },
+        full_name: { col: 280, row: 375 }
     },
-    sign_manager:{
-        signature:{col:470,row:330},
-        full_name:{col:450,row:375}
+    sign_manager: {
+        signature: { col: 470, row: 330 },
+        full_name: { col: 450, row: 375 }
     }
 };
 
@@ -66,8 +66,8 @@ var selectTable = () => {
 
 
 var createDatabase = () => {
-//neu ton tai file thi delete
-//sau do tao lai database
+    //neu ton tai file thi delete
+    //sau do tao lai database
 
 
     const db = require('./db/sqlite3/sqlite-hoadon-service.js');
@@ -126,7 +126,7 @@ var selectCustomer = () => {
 }
 
 
-var selectInvoice = (cust_id,cycle_id) => {
+var selectInvoice = (cust_id, cycle_id) => {
     const db = require('./db/sqlite3/sqlite-hoadon-service.js');
 
     setTimeout(() => {
@@ -154,45 +154,45 @@ var selectInvoice = (cust_id,cycle_id) => {
                 and customers.cust_id = invoices.cust_id  \
                 and customers.cust_id = invoices.cust_id \
                 ')
-                .then(results => {
-                    console.log('invoices', results);
-                    bill_array = results;
-                })
-                .catch(err => {
-                    console.log(err);
-                });
-                
-                
-                var saler;
-                db.db.getRst(
-                    '\
+            .then(results => {
+                console.log('invoices', results);
+                bill_array = results;
+            })
+            .catch(err => {
+                console.log(err);
+            });
+
+
+        var saler;
+        db.db.getRst(
+            '\
                     select description as full_name, signature from parameters where id = 33\
                     ')
-                    .then(results => {
-                        console.log('saler', results);
-                        saler = results;
-                    })
-                    .catch(err => {
-                        console.log(err);
-                    });
-                    
-                    var manager;
-                    db.db.getRst(
-                        '\
+            .then(results => {
+                console.log('saler', results);
+                saler = results;
+            })
+            .catch(err => {
+                console.log(err);
+            });
+
+        var manager;
+        db.db.getRst(
+            '\
                         select description as full_name, signature from parameters where id = 32\
                         ')
-                        .then(results => {
-                            console.log('manager', results);
-                            manager = results;
-                        })
-                        .catch(err => {
-                            console.log(err);
-                        });
-                        
-                        var bill_details;
-                        
-                        db.db.getRsts(
-                            '\
+            .then(results => {
+                console.log('manager', results);
+                manager = results;
+            })
+            .catch(err => {
+                console.log(err);
+            });
+
+        var bill_details;
+
+        db.db.getRsts(
+            '\
                             select              \
                             bills.cust_id             \
                             ,prices.unit         \
@@ -204,11 +204,11 @@ var selectInvoice = (cust_id,cycle_id) => {
                             and bills.bill_cycle = \''+ cycle_id + '\'         \
                             and bills.price_id = prices.id \
                             ')
-                            .then(results => {
-                                console.log('bill_details', results);
-                                bill_details = results;
-                            })
-                            .catch(err => {
+            .then(results => {
+                console.log('bill_details', results);
+                bill_details = results;
+            })
+            .catch(err => {
                 console.log(err);
             });
 
@@ -266,30 +266,30 @@ var selectInvoices = (bill_cycle) => {
         .catch(err => {
             console.log(err);
         });
-        
-        
-        var saler;
-        db.db.getRst('select description as full_name, signature from parameters where id = 33')
-            .then(results => {
-                saler = results;
-            })
-            .catch(err => {
-                console.log(err);
-            });
-            
-            var manager;
-            db.db.getRst('select description as full_name, signature from parameters where id = 32')
-                .then(results => {
-                    manager = results;
-                })
-                .catch(err => {
-                    console.log(err);
-                });
-                
-                var bill_details;
-                
-                db.db.getRsts(
-                    '\
+
+
+    var saler;
+    db.db.getRst('select description as full_name, signature from parameters where id = 33')
+        .then(results => {
+            saler = results;
+        })
+        .catch(err => {
+            console.log(err);
+        });
+
+    var manager;
+    db.db.getRst('select description as full_name, signature from parameters where id = 32')
+        .then(results => {
+            manager = results;
+        })
+        .catch(err => {
+            console.log(err);
+        });
+
+    var bill_details;
+
+    db.db.getRsts(
+        '\
                     select              \
                     bills.cust_id             \
                     ,prices.unit         \
@@ -298,79 +298,172 @@ var selectInvoices = (bill_cycle) => {
                     ,bills.total_not_vat \
                     from bills,prices where bill_cycle= '+ bill_cycle + ' and bills.price_id = prices.id \
                     ')
-                    .then(results => {
-                        bill_details = results;
+        .then(results => {
+            bill_details = results;
+        })
+        .catch(err => {
+            console.log(err);
+        });
+
+    setTimeout(() => {
+        // console.log('manager', manager);
+        // console.log('saler', saler);
+        // console.log('invoices', invoices);
+        // console.log('bill_details', bill_details);
+
+        if (manager && saler && invoices && bill_details) {
+            console.log('doc xong co du lieu'
+                , invoices.length
+                , bill_details.length
+                , manager
+                , saler
+            );
+
+            let invoicesPrint = []
+            invoices.forEach(el => {
+                el.bill_details = bill_details.filter(x => x.cust_id == el.cust_id);
+                el.bill_sum_charge_spell = utils.StringVietnamDong(el.sum_charge);
+                el.bill_sum = {
+                    sum_not_vat: el.sum_not_vat,
+                    sum_vat: el.sum_vat,
+                    sum_charge: el.sum_charge
+                };
+                el.sign_customer = {
+                    signature: null,
+                    full_name: el.full_name
+                }
+                el.sign_saler = {
+                    signature: saler.signature,
+                    full_name: saler.full_name
+                }
+                el.sign_manager = {
+                    signature: manager.signature,
+                    full_name: manager.full_name
+                }
+                invoicesPrint.push(el);
+            })
+
+            // console.log('invoicesPrint',invoicesPrint);
+
+            let printMatrixs = [];
+            invoicesPrint.forEach(el => {
+                printMatrixs.push(utils.GetMatrix(billPrintMatrix, el, { col: 0, row: 0 }));
+            });
+
+            //dung ma tran nay de tao pdf cuoi cung
+            console.log('printMatrixs', printMatrixs);
+
+        }
+
+    }, 3000);
+}
+
+//selectInvoices(201901);
+
+
+//viet ham tao hoa don,
+//bien vao la bill_cycle
+//neu khach hang mua san pham nao thi chon gia san pham 
+//so luong, don gia
+//khach hang chon 1 san pham, so luong la 1, loai gia 1 (co don gia, nhan ra, thoi gian mua hang)
+//ghep: customers (trang thai hien tai, khai gia, )
+const json2SqliteSQLInsert = (tablename, json) => {
+    let jsonInsert = { name: tablename, cols: [] }
+    for (let key in json) {
+        let col = { name: key, value: json[key] };
+        jsonInsert.cols.push(col);
+    }
+    return jsonInsert;
+}
+
+
+var createInvoices = (bill_cycle,bill_date,invoice_no)=>{
+    const db = require('./db/sqlite3/sqlite-hoadon-service.js');
+
+    var customers;
+    db.db.getRst('select customer_id, price_id, area_id, staff_id from customers where status=1 and id=1')
+        .then(results => {
+            customers = results;
+        })
+        .catch(err => {
+            console.log(err);
+        });
+    
+    var prices;
+    db.db.getRst('select id	,product_id, unit, not_vat ,vat, charge from prices where status = 1')
+            .then(results => {
+                prices = results;
+            })
+            .catch(err => {
+                console.log(err);
+            });
+
+    setTimeout(()=>{
+        if (customers&&prices){
+            console.log('doc xong du lieu tao', customers.length, prices.length);
+            customers.forEach(el=>{
+                
+                let product_count = 1; //so luong
+                let price = prices.find(x => x.id = el.price_id);
+
+                let bill_detail = {
+                    cust_id         : el.cust_id,          //khach mua
+                    bill_cycle      : bill_cycle,    //ky mua
+                    product_count   : product_count, //so luong
+                    price_id        : el.price_id,        //gia
+                    price_not_vat   : price.not_vat,
+                    total_not_vat   : price.not_vat * product_count,
+                    total_vat       : price.vat * product_count
+                };
+
+                let sqlBill = json2SqliteSQLInsert('bills', bill_detail);
+
+                db.db.insert(sqlBill)
+                    .then(data => {
+                        console.log(data);
                     })
                     .catch(err => {
                         console.log(err);
-                    });
-                    
-                    setTimeout(() => {
-                        // console.log('manager', manager);
-                        // console.log('saler', saler);
-                        // console.log('invoices', invoices);
-                        // console.log('bill_details', bill_details);
+                        //contraint thi update
 
-                        if (manager&&saler&&invoices&&bill_details){
-                            console.log('doc xong co du lieu'
-                            ,invoices.length
-                            ,bill_details.length
-                            ,manager
-                            ,saler
-                            );
+                    })
 
-                            let invoicesPrint=[]
-                            invoices.forEach(el=>{
-                                el.bill_details = bill_details.filter(x=>x.cust_id==el.cust_id);
-                                el.bill_sum_charge_spell = utils.StringVietnamDong(el.sum_charge);
-                                el.bill_sum = {
-                                    sum_not_vat: el.sum_not_vat,
-                                    sum_vat: el.sum_vat,
-                                    sum_charge: el.sum_charge
-                                };
-                                el.sign_customer = {
-                                    signature: null,
-                                    full_name: el.full_name
-                                }
-                                el.sign_saler = {
-                                    signature: saler.signature,
-                                    full_name: saler.full_name
-                                }
-                                el.sign_manager = {
-                                    signature: manager.signature,
-                                    full_name: manager.full_name
-                                }
-                                invoicesPrint.push(el);
-                            })
+                    let bill_sum={};
+                    bill_sum.sum_not_vat    = price.not_vat * product_count;
+                    bill_sum.sum_vat        = price.vat * product_count;
+                    bill_sum.sum_charge     = price.charge * product_count
 
-                           // console.log('invoicesPrint',invoicesPrint);
-                           
-                           let printMatrixs = [];
-                           invoicesPrint.forEach(el=>{
-                               printMatrixs.push(utils.GetMatrix(billPrintMatrix,el,{col:0,row:0}));
-                            });
-                            
-                            //dung ma tran nay de tao pdf cuoi cung
-                            console.log('printMatrixs',printMatrixs);
+                    let invoice = {
+                        cust_id         : el.cust_id
+                        , bill_cycle    : bill_cycle
+                        , bill_date     : bill_date
+                        , invoice_no    : invoice_no++
+                        , sum_not_vat   : bill_sum.sum_not_vat
+                        , sum_vat       : bill_sum.sum_vat
+                        , sum_charge    : bill_sum.sum_charge
+                    };
 
-                        }
-                        
-                    }, 3000);
-                }
+                    let sqlInvoice = json2SqliteSQLInsert('invoices', invoice);
 
- //selectInvoices(201901);
+                    db.db.insert(sqlInvoice)
+                        .then(data => {
+                            console.log(data);
+                            return invoice_no;
+                        })
+                        .catch(err => {
+                            console.log(err);
+                            //contraint thi update
+
+                        })
+                
+            })
+        }
+    },1000);
+}
 
 
- //viet ham tao hoa don,
- //bien vao la bill_cycle
- //neu khach hang mua san pham nao thi chon gia san pham 
- //so luong, don gia
- //khach hang chon 1 san pham, so luong la 1, loai gia 1 (co don gia, nhan ra, thoi gian mua hang)
- //ghep: customers (trang thai hien tai, khai gia, )
- var createInvoice = (cust_id,bill_cycle,price_id,product_count)=>{
-     //tao bill_details
-     //tao invoice tu bill
-     //chen hoac update vao csdl
-     
+let invoice_no = 1;
 
- }
+invoice_no = createInvoices(201901,20190120,invoice_no);
+
+console.log(invoice_no);
