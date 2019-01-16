c4619b611223fc0dc91557a569baaa5fb609b431
cuongdqpayment <cuongdq.payment@gmail.com>
2019-01-16 17:14:08 +0700

    cuongdq push by sh

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 import { TabsPage } from '../pages/tabs/tabs';
 import { LoginPhonePage } from '../pages/login-phone/login-phone';
 import { HomePage } from '../pages/home/home';
+import { ConfigPage } from '../pages/config/config';
 
 
 
