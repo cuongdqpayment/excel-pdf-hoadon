03a492758d5c8dd831df9fe25f16416848efc0e8
cuongdqpayment <cuongdq.payment@gmail.com>
2019-01-21 14:22:01 +0700

    cuongdq push by sh

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
+import { Platform, AlertController } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { TabsPage } from '../pages/tabs/tabs';
@@ -14,18 +14,53 @@ import { ConfigPage } from '../pages/config/config';
 })
 export class MyApp {
   rootPage:any = LoginPhonePage;
-  isWeb:boolean = false;
-  
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  platforms = ['android', 'cordova', 'core', 'ios', 'ipad', 'iphone', 'mobile', 'mobileweb', 'phablet', 'tablet', 'windows', 'electron'];
+  isWeb:boolean = false;        //platform.is('core');
+  isWebMobile:boolean = false;  //platform.is('mobile');
+  isApp: boolean = false;       //platform.is('cordova');
+  isIos: boolean = false;       //platform.is('ios'); //he dieu hanh ios
+  isAndroid: boolean = false;   //platform.is('ios'); //he dieu hanh ios
+
+
+  constructor(private platform: Platform,
+              private statusBar: StatusBar,
+              private alertCtrl: AlertController,
+              private splashScreen: SplashScreen) {
     platform.ready().then(() => {
       statusBar.styleDefault();
       splashScreen.hide();
-      if (  platform.is('mobileweb') 
-         || platform.platforms()[0] == 'core'){
-        //version web
+      if (platform.platforms()[0] === 'core'){
         this.isWeb = true;
       }
+      
+      let currentPlatform = [];
+
+      this.platforms.forEach(e=>{
+        if (platform.is(e)) currentPlatform.push(e); 
+      })
+
+      this.presentAlert({
+        title:"PLATFORM is",
+        message: 'Platform is ' + JSON.stringify(currentPlatform) + '<br> platform.platforms(): ' +JSON.stringify(platform.platforms()),
+        ok_text:'OK'
+      })
+
     });
   }
+
+  presentAlert(jsonConfirm:{title:string,message:string,ok_text:string}) {
+    let alert = this.alertCtrl.create({
+      title: jsonConfirm.title, //'Xác nhận phát hành',
+      message: jsonConfirm.message, //'Bạn muốn ',
+      buttons: [
+        {
+          text: jsonConfirm.ok_text,//'Đồng ý',
+          handler: () => {}
+        }
+      ]
+    });
+    alert.present();
+  }
+
 }
 
