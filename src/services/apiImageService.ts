33838c2bf22311a74a8eb96dcd241e8b6b89d791
cuongdq.payment <cuongdq.payment@gmail.com>
2019-02-10 20:54:10 +0700

    cuongdq push by sh

--- src/services/apiImageService.ts
+++ src/services/apiImageService.ts
@@ -25,10 +25,11 @@ export class ApiImageService {
                 try {
                     let canvas = document.createElement('canvas');
                     let context = canvas.getContext('2d');
+                    let img = document.createElement('img');
                     let maxW = newSize;
                     let maxH = newSize;
-                    let img = document.createElement('img');
                     img.src = URL.createObjectURL(file);
+                    
                     img.onload = () => {
                         let iw = img.width;
                         let ih = img.height;
@@ -36,63 +37,65 @@ export class ApiImageService {
                         let iwScaled = iw * scale;
                         let ihScaled = ih * scale;
                         
-                        if (originOrientation>2&&originOrientation<=4){
-                            //rotate 180
-                            console.log('rotate 180');
-                            canvas.width = img.width;
-                            canvas.height = img.height;
-                            context.rotate(180 * Math.PI / 180);
-                            context.drawImage(img, -img.width, -img.height);
-
-                        } else if (originOrientation>4&&originOrientation<=6){
-                            //rotate 90
-                            console.log('rotate 90');
-                            canvas.width = img.height;
-                            canvas.height = img.width;
-                            context.rotate(90 * Math.PI / 180);
-                            context.drawImage(img, 0 , -img.height);
-
-                        } else if (originOrientation>6&&originOrientation<=8){
-                            //rotate 270
-                            canvas.width = img.height;
-                            canvas.height = img.width;
-                            context.rotate(270 * Math.PI / 180);
-                            context.drawImage(img, -img.width, 0);
-
-                        }else{
-                            //rotate 0
-                            console.log('no rotate');
-                            canvas.width = iwScaled;
-                            canvas.height = ihScaled;
-                            context.drawImage(img, 0, 0, iwScaled, ihScaled);
+                        //giam kich thuoc
+                        canvas.width = iwScaled;
+                        canvas.height = ihScaled;
+                        context.drawImage(img, 0, 0, iwScaled, ihScaled);
+                        
+                        //quay
+                        let imageNew = document.createElement('img');
+                        imageNew.src = canvas.toDataURL();
+                        
+                        imageNew.onload = () => {
                             
+                            if (originOrientation>2&&originOrientation<=4){
+                                //console.log('rotate 180');
+                                canvas.width = imageNew.width;
+                                canvas.height = imageNew.height;
+                                context.rotate(180 * Math.PI / 180);
+                                context.drawImage(imageNew, -imageNew.width, -imageNew.height);
+                                
+                            } else if (originOrientation>4&&originOrientation<=6){
+                                //rotate 90
+                                //console.log('rotate 90');
+                                canvas.width = imageNew.height;
+                                canvas.height = imageNew.width;
+                                context.rotate(90 * Math.PI / 180);
+                                context.drawImage(imageNew, 0 , -imageNew.height);
+                                
+                            } else if (originOrientation>6&&originOrientation<=8){
+                                //rotate 270
+                                //console.log('rotate 270');
+                                canvas.width = imageNew.height;
+                                canvas.height = imageNew.width;
+                                context.rotate(270 * Math.PI / 180);
+                                context.drawImage(imageNew, -imageNew.width, 0);
+    
+                            }
+                            
+                            canvas.toBlob((blob) => {
+                                let reader = new FileReader();
+                                reader.readAsArrayBuffer(blob);
+                                reader.onload = () => { 
+                                    let newFile = new Blob([reader.result], { type: 'image/jpeg' });
+                                    resolve({
+                                        image: canvas.toDataURL(), //base64 for view and json post
+                                        file: newFile //formData post
+                                        ,title: filename
+                                        ,last_modified: file.lastModifiedDate
+                                        ,subtitle: file.lastModifiedDate + "("+ originOrientation +")"
+                                        ,width: canvas.width //cho biet anh nam doc hay nam ngang
+                                        ,height: canvas.height 
+                                        ,orientation_old: originOrientation
+                                        ,size_old: file.size
+                                        ,type_old: file.type
+                                        ,size: newFile.size
+                                        ,type: newFile.type
+                                    });
+                                }
+                            });
                         }
                         
-
-
-
-                        
-
-                        canvas.toBlob((blob) => {
-                            let reader = new FileReader();
-                            reader.readAsArrayBuffer(blob);
-                            reader.onload = () => { 
-                                let newFile = new Blob([reader.result], { type: 'image/jpeg' });
-                                resolve({
-                                    image: canvas.toDataURL(), //base64 for view and json post
-                                    file: newFile //formData post
-                                    ,title: filename
-                                    ,last_modified: file.lastModifiedDate
-                                    ,subtitle: file.lastModifiedDate + "("+ originOrientation +")"
-                                    ,width: canvas.width //cho biet anh nam doc hay nam ngang
-                                    ,height: canvas.height 
-                                    ,size_old: file.size
-                                    ,type_old: file.type
-                                    ,size: newFile.size
-                                    ,type: newFile.type
-                                });
-                            }
-                        });
                     }
                 } catch (err) { reject(err);}
             }else{
