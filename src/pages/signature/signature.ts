5e92587a6de9bdb953679578337810859f772182
cuongdq.payment <cuongdq.payment@gmail.com>
2019-02-13 11:27:22 +0700

    cuongdq push by sh

---
+++ src/pages/signature/signature.ts
@@ -0,0 +1,80 @@
+import { Component, ViewChild, Renderer } from '@angular/core';
+import { Platform, NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-signature',
+  templateUrl: 'signature.html'
+})
+export class SignaturePage {
+  @ViewChild('myCanvas') canvas: any;
+
+  canvasElement: any;
+  lastX: number;
+  lastY: number;
+
+  currentColour: string = '#1abc9c';
+  availableColours: any;
+  brushSize: number = 10;
+
+  constructor(
+    private platform: Platform
+    , private renderer: Renderer
+    , private navCtrl: NavController) {
+    
+    this.availableColours = [ '#1abc9c',
+                              '#3498db',
+                              '#9b59b6',
+                              '#e67e22',
+                              '#e74c3c'
+                            ];
+  }
+
+  ngAfterViewInit() {
+
+    this.canvasElement = this.canvas.nativeElement;
+
+    this.renderer.setElementAttribute(this.canvasElement, 'width', this.platform.width() + '');
+    this.renderer.setElementAttribute(this.canvasElement, 'height', this.platform.height() + '');
+  }
+
+  changeColour(colour) {
+    this.currentColour = colour;
+  }
+
+  changeSize(size) {
+    this.brushSize = size;
+  }
+
+  handleStart(ev) {
+
+    this.lastX = ev.touches[0].pageX;
+    this.lastY = ev.touches[0].pageY;
+  }
+
+  handleMove(ev) {
+
+    let ctx = this.canvasElement.getContext('2d');
+    let currentX = ev.touches[0].pageX;
+    let currentY = ev.touches[0].pageY;
+
+    ctx.beginPath();
+    ctx.lineJoin = "round";
+    ctx.moveTo(this.lastX, this.lastY);
+    ctx.lineTo(currentX, currentY);
+    ctx.closePath();
+    ctx.strokeStyle = this.currentColour;
+    ctx.lineWidth = this.brushSize;
+    ctx.stroke();
+
+    this.lastX = currentX;
+    this.lastY = currentY;
+  }
+  clearCanvas() {
+    let ctx = this.canvasElement.getContext('2d');
+    ctx.clearRect(0, 0, this.canvasElement.width, this.canvasElement.height);
+  }
+
+  showImage() {
+   
+  }
+}
